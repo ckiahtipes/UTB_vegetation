a14ac2927d041000278cf088afb64a07ffe19cb2
ckiahtipes <chris.kiahtipes@gmail.com>
2025-11-03 15:56:13 -0500

    Added logical control and output controls.

--- FL_county-map.R
+++ FL_county-map.R
@@ -11,6 +11,10 @@ library(sf)
 library(png)
 data("us_states")
 
+#Set some logical variables
+
+save_figs = TRUE
+
 #Let's read a shapefile with couty polygons.
 
 counties = read_sf("mapping/cb_2024_us_county_500k/cb_2024_us_county_500k.shp")
@@ -24,6 +28,11 @@ FL_counties = counties[FL, ]
 
 #Plot outlines and add Hillsborough county as fill. This is where you can pull from a file or list of some kind to automate.
 
+if(save_figs == TRUE){
+  setEPS()
+  tiff("FL_specimen-map.tiff", height = 2200, width = 2300, res = 300)
+}
+
 plot(0, 0, xlim = c(-88, -78), ylim = c(24,32), axes = FALSE, ann = FALSE)
 plot(FL_counties$geometry, add = TRUE)
 plot(FL_counties$geometry[FL_counties$NAME == "Hillsborough"], add = TRUE, col = "red")
@@ -31,3 +40,7 @@ plot(FL_counties$geometry[FL_counties$NAME == "Hillsborough"], add = TRUE, col =
 #Add herbarium logo - needs adjustment.
 
 rasterImage(logo, -81,29,-79,31)
+
+if(save_figs == TRUE){
+  dev.off()
+}
