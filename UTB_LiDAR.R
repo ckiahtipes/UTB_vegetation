b24e7bfa9bc5680c1cc0f783bca192279ad46a4b
Chris Kiahtipes <greeksciencecorp@Chriss-Mac-Studio.local>
2025-12-04 06:39:27 -0500

    Added landcover.

--- UTB_LiDAR.R
+++ UTB_LiDAR.R
@@ -11,6 +11,14 @@ library(oce)
 
 #Load and make raster
 
-landcover_path = "~/Dropbox/Temporary/UTB_Plant-Inventory/UTB_code/maps/UTB_LiDAR-DEM_2019_ngs_tampabay_topobath_dem_J1287063/UTB_LiDAR-DEM_2019_ngs_tampabay_topobath_dem_J1287063tR0_C0.tif" 
+LiDAR_path = "~/Dropbox/Temporary/UTB_Plant-Inventory/UTB_code/maps/UTB_LiDAR-DEM_2019_ngs_tampabay_topobath_dem_J1287063/UTB_LiDAR-DEM_2019_ngs_tampabay_topobath_dem_J1287063tR0_C0.tif" 
+landcover_path = "~/Dropbox/Temporary/UTB_Plant-Inventory/UTB_code/maps/UTB_LandCover2021_2021_TampaFL_HighResLandCoverv2_J1287129/UTB_LandCover2021_2021_TampaFL_HighResLandCoverv2_J1287129tR0_C0.tif"
 
+
+LiDAR_rast = rast(LiDAR_path)
 landcover_rast = rast(landcover_path)
+
+landcover_mod = project(landcover_rast, "EPSG:6443", method = "near")
+
+plot(landcover_mod)
+plot(LiDAR_rast, add = TRUE, alpha = 0.75, col = "grayscale")
