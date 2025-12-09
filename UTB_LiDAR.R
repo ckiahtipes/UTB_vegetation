040e2eea7461458f3c9ac313bceba392785335e8
Chris Kiahtipes <greeksciencecorp@Chriss-Mac-Studio.local>
2025-12-09 06:47:11 -0500

    Updated mapping of UTB with LiDAR contours and added mapping scripts for Weedon Island.

--- UTB_LiDAR.R
+++ UTB_LiDAR.R
@@ -8,17 +8,38 @@ library(terra)
 library(spData)
 library(spDataLarge)
 library(oce)
+library(tmap)
 
 #Load and make raster
 
-LiDAR_path = "~/Dropbox/Temporary/UTB_Plant-Inventory/UTB_code/maps/UTB_LiDAR-DEM_2019_ngs_tampabay_topobath_dem_J1287063/UTB_LiDAR-DEM_2019_ngs_tampabay_topobath_dem_J1287063tR0_C0.tif" 
-landcover_path = "~/Dropbox/Temporary/UTB_Plant-Inventory/UTB_code/maps/UTB_LandCover2021_2021_TampaFL_HighResLandCoverv2_J1287129/UTB_LandCover2021_2021_TampaFL_HighResLandCoverv2_J1287129tR0_C0.tif"
+UTB_LiDAR_path = "~/Dropbox/Temporary/UTB_Plant-Inventory/UTB_code/maps/UTB_LiDAR-DEM_2019_ngs_tampabay_topobath_dem_J1287063/UTB_LiDAR-DEM_2019_ngs_tampabay_topobath_dem_J1287063tR0_C0.tif" 
+UTB_landcover_path = "~/Dropbox/Temporary/UTB_Plant-Inventory/UTB_code/maps/UTB_LandCover2021_2021_TampaFL_HighResLandCoverv2_J1287129/UTB_LandCover2021_2021_TampaFL_HighResLandCoverv2_J1287129tR0_C0.tif"
 
+WDI_LiDAR_path0 = "~/Dropbox/Temporary/UTB_Plant-Inventory/UTB_code/maps/LiDAR_DEM_2019_ngs_topobathy_tampaBay_J1292430/LiDAR_DEM_2019_ngs_topobathy_tampaBay_J1292430tR0_C0.tif"
+WDI_LiDAR_path1 = "~/Dropbox/Temporary/UTB_Plant-Inventory/UTB_code/maps/LiDAR_DEM_2019_ngs_topobathy_tampaBay_J1292430/LiDAR_DEM_2019_ngs_topobathy_tampaBay_J1292430tR0_C1.tif"
+WDI_LiDAR_path2 = "~/Dropbox/Temporary/UTB_Plant-Inventory/UTB_code/maps/LiDAR_DEM_2019_ngs_topobathy_tampaBay_J1292430/LiDAR_DEM_2019_ngs_topobathy_tampaBay_J1292430tR1_C0.tif"
+WDI_LiDAR_path3 = "~/Dropbox/Temporary/UTB_Plant-Inventory/UTB_code/maps/LiDAR_DEM_2019_ngs_topobathy_tampaBay_J1292430/LiDAR_DEM_2019_ngs_topobathy_tampaBay_J1292430tR1_C1.tif"
+WDI_landcover_path = "~/Dropbox/Temporary/UTB_Plant-Inventory/UTB_code/maps/Land_Cover_Weedon_Island_2021_TampaFL_HighResLandCoverv2_J1292431/Land_Cover_Weedon_Island_2021_TampaFL_HighResLandCoverv2_J1292431tR0_C0.tif"
 
-LiDAR_rast = rast(LiDAR_path)
-landcover_rast = rast(landcover_path)
+UTB_LiDAR_rast = rast(UTB_LiDAR_path)
+UTB_landcover_rast = rast(UTB_landcover_path)
 
-landcover_mod = project(landcover_rast, "EPSG:6443", method = "near")
+WDI_LiDAR_rast0 = rast(WDI_LiDAR_path0)
+WDI_LiDAR_rast1 = rast(WDI_LiDAR_path1)
+WDI_LiDAR_rast2 = rast(WDI_LiDAR_path2)
+WDI_LiDAR_rast3 = rast(WDI_LiDAR_path3)
 
-plot(landcover_mod)
-plot(LiDAR_rast, add = TRUE, alpha = 0.75, col = "grayscale")
+WDI_LiDAR_group = sprc(WDI_LiDAR_rast0, WDI_LiDAR_rast1, WDI_LiDAR_rast2, WDI_LiDAR_rast3)
+WDI_LiDAR_rast = mosaic(WDI_LiDAR_group)
+WDI_landcover_rast = rast(WDI_landcover_path)
+
+UTB_landcover_mod = project(UTB_landcover_rast, "EPSG:6443", method = "near")
+WDI_landcover_mod = project(WDI_landcover_rast, "EPSG:6443", method = "near")
+
+plot(UTB_landcover_mod)
+plot(UTB_LiDAR_rast, breaks=25, col=gray.colors(25), reset=FALSE, alpha = 0.50, add = TRUE, legend = FALSE)
+contour(UTB_LiDAR_rast, add = TRUE, nlevels = 20)
+
+plot(WDI_landcover_mod)
+plot(WDI_LiDAR_rast, breaks = 25, col=gray.colors(25), reset = FALSE, alpha = 0.5, add = TRUE, legend = FALSE)
+contour(WDI_LiDAR_rast, add = TRUE, nlevels = 20)
