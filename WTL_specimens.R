462435cd8b994ba639699b9961ef433fcb2d09bd
ckiahtipes <chris.kiahtipes@gmail.com>
2026-02-05 15:52:26 -0500

    Working on improvements to specimen list.

--- WTL_specimens.R
+++ WTL_specimens.R
@@ -1,6 +1,6 @@
 #Withlacoochee Data Exploration
 
-WTL_sp <- read.csv("WTL_specimens.csv", header = TRUE, row.names = "barcode")
+WTL_sp <- read.csv("WTL_specimens.csv", header = TRUE)
 
 par(mar = c(5, 6, 4, 2) + 0.1)
 
@@ -20,4 +20,44 @@ par(mar = c(5, 8, 4, 2) + 0.1)
 
 barplot(collector[order(collector)], horiz = TRUE, las = 1, main = "N 'Withlacoochee' Specimens in USF Herbarium by Collector", cex.text = 0.8)
 
-par(mar = c(5, 4, 4, 2) + 0.1)
+par(mar = c(5, 8, 4, 2) + 0.1)
+
+barplot(table(WTL_sp$genus), horiz = TRUE, las = 1, main = "N Withlacoochee Specimens by Genera")
+
+par(mar = c(5, 8, 4, 2) + 0.1)
+
+#Okay, time for some more serious aggregation work. Need Family, Genus, Species, Collector, and Collection Date.
+
+WTL_sp <- read.csv("WTL_specimens.csv", header = TRUE) #Reading "Withlacoochee" as Location
+WSF_sp = read.csv("WSF_specimens.csv", header = TRUE) #Reading "Withlacoochee State Forest as Location"
+
+comb_sp = rbind(WTL_sp, WSF_sp)
+
+doubles_check = table(comb_sp$barcode)
+
+cut_list = doubles_check[doubles_check > 1]
+
+for(i in 1:length(cut_list)){
+  cut = grep(names(cut_list[i]), comb_sp$barcode)
+  comb_sp = comb_sp[-c(cut[1], cut[2]),]
+  pull = WTL_sp[cut_list[i],]
+  comb_sp = rbind(comb_sp, pull)
+}
+
+WTL_taxa = data.frame(comb_sp$family, comb_sp$genus, comb_sp$species)
+
+WTL_dates = comb_sp$Collection.Date
+
+coll_dates = as.Date(comb_sp$Collection.Date, format = "%d/%m/%Y")
+coll_yr = as.Date(coll_dates, format = "%Y")
+
+#We've extracted collection years, going to go back and backfill some of the shitty data.
+#Should probably do this after we've got an official export from the database.
+
+WTL_data = cbind(WTL_taxa, coll_yr)
+
+test = table(WTL_data$coll_yr, WTL_data$comb_sp.genus)
+
+
+
+
